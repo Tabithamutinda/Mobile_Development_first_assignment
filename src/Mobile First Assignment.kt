e9a47639cdce70f52ef9f83bafec3524be68b9c1
Tabithamutinda <tabbithamutinda@gmail.com>
2021-02-21 10:53:51 +0300

    first commit

---
+++ src/Mobile First Assignment.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name:String="Tabitha Mutinda"
+    val age:Int=19
+    val phoneNumber="0740918285"
+    val citizenKenyan:Boolean=true
+    val weight:Int=50
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(citizenKenyan)
+    println(weight)
+}
